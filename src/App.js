8d136734b17853f637935eed0765b7530600b8e9
Pavithra Murugan <mpavithracse@gmail.com>
2022-02-04 08:07:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
 
 function App() {
+const details = [
+  {
+    subname : "FREE",
+    cost : "$0",
+    canget:["Single User","5GB Storage","Unlimited Public Projects",
+    "Community Access"],
+    cantget: ["Dedicated Phine Support","Free Subdomain","Monthly Status Reports",
+    "Unlimites Private Projects"]
+  },
+  {
+    subname : "PLUS",
+    cost : "$9",
+    canget:["5 Users","50GB Storage","Unlimited Public Projects",
+    "Community Access","Unilimited Private Projects",
+    "Dedicated Phone support","Free Subdomain"],
+    cantget: ["Monthly Status Reports"]
+  },
+  {
+    subname : "PRO",
+    cost : "$40",
+    canget:["Unlimited Users","150GB Storage",
+    "Unlimites Public Projects","Community Access",
+    "Unlimited Private Projects","Dedicated Phone Support",
+    "Unlimited Free Subdomains","Monthly Status Reports"],
+    cantget: []
+  }
+]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      {details.map(({subname,cost,canget,cantget}) =>(
+        <div className='Price-tag'>
+        <div className='price-part'>
+           {subname}<br/>
+         <p><b>{cost}</b>/month</p>
+         </div>
+         <hr/>
+         <ul>
+           {canget.map((value)=>(
+             <li><DoneIcon/>{value}</li>
+           ))}
+           {cantget.map((value)=>(
+             <li><CloseIcon/>{value}</li>
+           ))}         
+         </ul>
+         <Button variant="contained" className = 'ButtonUI'>Button</Button>
+      </div>
+      ))}
     </div>
   );
-}
+  }
 
 export default App;
